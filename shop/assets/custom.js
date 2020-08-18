fbb8d04d03fd7c303858d97e37fcfca9db417cdf
Pedro <pedromatanzo@gmail.com>
2020-08-19 00:47:13 +0200

    fournisseurs map mobile

--- shop/assets/custom.js
+++ shop/assets/custom.js
@@ -22,3 +22,28 @@
  *   bubbles: true
  * }));
  */
+
+ $(document).ready(function() {
+    //toggle the component with class accordion_body
+    $(".accordion_head").click(function() {
+      $(this).removeClass('coll-back');
+      if ($('.accordion_body').is(':visible')) {
+        $(".accordion_body").slideUp(300);
+        $(".plusminus").text('+');
+        $(this).removeClass('coll-back');
+        $('.rmv-cls').removeClass('coll-back');
+      }
+
+      if($(this).next(".accordion_body").is(':visible')) {
+        $(this).next(".accordion_body").slideUp(300);
+        $(this).children(".plusminus").text('+');
+        $(this).removeClass('coll-back');
+      }else {
+        $(this).next(".accordion_body").slideDown(300);
+        $(this).children(".plusminus").text('');
+        $(this).children(".plusminus").append('<hr class="hr-clc">');
+        $(this).toggleClass('coll-back');
+        $(this).addClass('rmv-cls');
+      }
+    });
+  });
